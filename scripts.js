0d816c28571f8bb791dd123b24284ec1ffb74182
Sean <sean@Sens-MacBook-Pro.local>
2017-08-17 17:25:57 -0500

    completed nesting

--- scripts.js
+++ scripts.js
@@ -1,74 +1,78 @@
-//Queston 1 & 2
-function AnimalTestUser(username) {
-  var argLength = arguments.length;
-  var otherArgs = [];
-  if (argLength > 1) {
-    for (var i = 1; i < argLength; i++) {
-      otherArgs.push(arguments[i])
-    }
-  }
-  return {
-    username: username,
-    otherArgs: otherArgs
-  }
+// from object exercise
+var animal = {};
+
+animal.username = "chris";
+
+animal["tagline"] = "I'm coco for cocoa puffs";
+
+var noises = [];
+animal['noises'] = noises
+
+console.log(animal);
+
+
+// Array Exercise ------------------------------
+
+var noiseArray = ['hiss'];
+
+noiseArray.unshift('nehh');
+noiseArray.push('ruff');
+noiseArray[3] = 'meow';
+
+console.log(noiseArray.length);
+// what is the length? answer: 4
+
+console.log(noiseArray[noiseArray.length - 1]);
+// what is the last index? how is it diff. than length?
+// answer: meow
+
+animal.noises = noiseArray;
+console.log(animal);
+
+
+// Animal Collection exercise -----------------------------
+// come up w/ two ways you can add an element to the end of an aray w/out knowing the exact length of the array. User --> arr.push or arr[arr.length]
+var animals = [];
+animals.push(animal)
+
+var quackers = { username: 'DaffyDuck', tagline: 'Yippeee!', noises: ['quack', 'honk', 'sneeze', 'growl'] }
+
+// pushing quackers at th end bracket notation
+animals[animals.length] = quackers;
+
+// pushing using .push method
+var cat = {
+  username: 'Meowith',
+  tagline: 'I love cat nip',
+  noises: ['meow', 'sneeze', 'purr']
 }
+animals.push(cat)
 
-var testSheep = AnimalTestUser('CottonBall');
-// console.log(testSheep); // {username: 'CottonBall'}
 
-var testSheep = AnimalTestUser('CottonBall', {'loves dancing': true}, [1,2,3]);
-console.log(testSheep);
+console.log(animals); // --> [{..},{..},{..}]
 
-// Qestion 3
-// construtor function ------------------------------------------------------------
-function AnimalCreator(username, species, tagline, noises) {
-  var animal = {
-    username: username,
-    species: species,
-    tagline: tagline,
-    noises: noises,
-    friends: []
-  }
-  return animal;
+
+
+//----------------------------Nesting----------------------------------------------
+
+
+var friends = [];
+
+friends.push(animals[0].username, animals[1].username)
+console.log(friends); // -->  array ['Chris', 'DaffyDuck']
+
+var relationships = {};
+
+relationships.friends = friends;
+console.log(relationships); // --> Object {friends: ['Chris', 'DaffyDuck']}
+console.log(Object.keys(relationships).length); // --> 1
+
+var matches = [];
+relationships.matches = matches;
+relationships.matches.push('pig');
+console.log(relationships); // --> Object {friends: [Chris, DaffyDuck], matches: ['pig']}
+
+for (var i = 0; i < animals.length; i++) {
+  animals[i].relationships = relationships
 }
-var sheep = AnimalCreator('Happy', 'dog', 'play all day', ['ruff', 'bow', 'wow'])
-console.log(sheep);
-
-// Question 4 & 5 ------------------------------------------------------------
-function addFriend(animal, friend) {
-  animal.friends.push(friend.username)
-}
-
-var cow = AnimalCreator('Harry', 'cow', 'got milk', ['moo', 'mooooo', 'eat more chicken']);
-console.log(cow);
-
-var llama = AnimalCreator('Tina', 'llama', 'got lasagna', ['spits', 'chew', 'sniffs'])
-addFriend(cow, sheep);
-addFriend(llama, cow)
-addFriend(sheep, llama)
-
-// Question 6
-var myFarm = [sheep, cow, llama];
-addFriend(cow, sheep);
-addFriend(llama, cow)
-console.log(myFarm);
-
-// Question 7-----------------------------------------------
-function addMatchesArray(farm){
-  for (var animal in farm){
-    farm[animal].matches = [];// creates a property matches in farm obj
-  }
-}
-
-addMatchesArray(myFarm)
-console.log(myFarm[0]);
-
-// Question 8
-function giveMatches(farm){
-  for (var animal in farm){
-    farm[animal].matches.push(farm[animal].friends[0]);
-  }
-}
-
-giveMatches(myFarm);
-console.log(myFarm[0]);
+console.log(animals); //--> [{...},{...},{...} relationships: ['..'],['..']]
